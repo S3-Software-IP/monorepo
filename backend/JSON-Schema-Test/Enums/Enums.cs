71df103285fd29260f26f86cc8cafb9bd71d113d
Brugman,Joris J.A <jorisplayz@gmail.com>
2023-10-12 15:09:12 +0200

    Added docker

---
+++ backend/JSON-Schema-Test/Enums/Enums.cs
@@ -0,0 +1,19 @@
+﻿namespace JSON_Schema_Test.Enums;
+
+public class Enums
+{
+
+    public enum RankingType
+    {
+        Artists,
+        Tracks,
+        Genres
+    }
+
+    public enum TimeRangeType
+    {
+        short_rage,
+        medium_range,
+        long_range
+    }
+}
---
+++ backend/JSON-Schema-Test/Rank.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JSON_Schema_Test;
+
+public class Rank
+{
+    [Required]
+    [MaxLength(64)]
+    public string Id { get; set; }
+}
