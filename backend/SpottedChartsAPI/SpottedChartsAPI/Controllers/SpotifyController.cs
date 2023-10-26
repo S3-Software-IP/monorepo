6c22648f4a602b0c5c5ebf3ce11cee0abc0d2c07
Morris Hannessen <510709@student.fontys.nl>
2023-10-26 13:06:21 +0200

    fixed api

---
+++ backend/SpottedChartsAPI/SpottedChartsAPI/Controllers/SpotifyController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace SpottedChartsAPI.Controllers
+{
+    public class SpotifyController : Controller
+    {
+        public enum TimeRange
+        {
+            short_term,
+            medium_term,
+            long_term
+        }
+
+        [HttpGet]
+        [Route("/api/spotify/gettoptracks")]
+        public async Task<IActionResult> GetTopTracks(string bearerToken, TimeRange timeRange = TimeRange.medium_term, int limit = 50)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
+                string timeRangeStr = timeRange.ToString().Replace("_", "");
+                string endpoint = $"https://api.spotify.com/v1/me/top/tracks?time_range={timeRangeStr}&limit={limit}";
+
+                HttpResponseMessage response = await httpClient.GetAsync(endpoint);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    return Ok(responseBody);
+                }
+                else
+                {
+                    return BadRequest("Failed to receive tracks");
+                }
+            }
+        }
+
+        [HttpGet]
+        [Route("/api/spotify/gettopartists")]
+        public async Task<IActionResult> GetTopArtists(string bearerToken, TimeRange timeRange = TimeRange.medium_term, int limit = 50)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
+                string timeRangeStr = timeRange.ToString().Replace("_", "");
+                string endpoint = $"https://api.spotify.com/v1/me/top/artists?time_range={timeRangeStr}&limit={limit}";
+
+                HttpResponseMessage response = await httpClient.GetAsync(endpoint);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    return Ok(responseBody);
+                }
+                else
+                {
+                    return BadRequest("Failed to receive tracks");
+                }
+            }
+        }
+
+        [HttpGet]
+        [Route("/api/spotify/getuserinfo")]
+        public async Task<IActionResult> GetUserInfo(string bearerToken)
+        {
+            using (var httpClient = new HttpClient())
+               {
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
+                string endpoint = "https://api.spotify.com/v1/me";
+                HttpResponseMessage response = await httpClient.GetAsync(endpoint);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    var userData = Newtonsoft.Json.JsonConvert.DeserializeObject<UserData>(responseBody);
+
+                    var userName = userData.Name;
+                    var userId = userData.Id;
+
+                    return Ok(new { Name = userName, Id = userId });
+                }
+                else
+                {
+                    return BadRequest("Failed to retrieve user info.");
+                }
+            }
+        }
+    }
+}
---
+++ backend/SpottedChartsAPI/SpottedChartsAPI/UserData.cs
@@ -0,0 +1,8 @@
+﻿namespace SpottedChartsAPI
+{
+    public class UserData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
